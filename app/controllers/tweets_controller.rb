1f9bc0fac8e7afda118337ae1cd4d7c9eeb9eb39
sheepdog0820 <sheepdgo1919@gmail.com>
2020-04-13 10:39:25 +0900

    createに保存ができたときにリダイレクトするように変更
    保存できた場合のレンダー処理

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,11 +1,27 @@
 class TweetsController < ApplicationController
+  before_action :set_tweet, only: [:destroy]
 
   def index
     @tweets = Tweet.includes(:user).order("created_at DESC").page(params[:page]).per(5)
   end
 
   def create
-    Tweet.create(tweet_params)
+    @tweet = Tweet.new(tweet_params)
+    if @tweet.save
+      redirect_to root_path
+    else
+      render :index
+    end
+  end
+
+
+  def destroy
+    if @tweet.destroy
+      redirect_to root_path, notice: '削除に成功しました。'
+    else
+      flash.now[:alert] = '削除に失敗しました。'
+      redirect_to root_path      
+    end
   end
 
   private
@@ -16,4 +32,9 @@ class TweetsController < ApplicationController
   def move_to_index
     redirect_to action: :index unless user_signed_in?
   end
+
+  def set_tweet
+    @tweet = Tweet.find(params[:id])
+  end
+
 end
